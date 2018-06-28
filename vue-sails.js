27a97015d465c2f8cca57f300ca0ccb9d1643fbb
Bin Ury <bin.ury@petrocloud.com>
2018-06-28 17:44:34 -0500

    Replace vanilla io with promisification wrapper

--- vue-sails.js
+++ vue-sails.js
@@ -1,23 +1,46 @@
+/* global define, window */
 (function() {
   'use strict';
 
+
   var plugin = {
     install: function(Vue, io) {
+      var promiseForRequest = (type, url, data) => new Promise(resolve => io[type](url, data, resolve));
+      var vueSails = {
+        get(url, data){
+          return promiseForRequest('get', url, data);
+        },
+        post(url, data){
+          return promiseForRequest('post', url, data);
+        },
+        put(url, data){
+          return promiseForRequest('put', url, data);
+        },
+        patch(url, data){
+          return promiseForRequest('patch', url, data);
+        },
+        delete(url, data){
+          return promiseForRequest('delete', url, data);
+        },
+        on: io.on,
+        off: io.off,
+
+      };
 
       // Every component will have this
       Vue.mixin({
         created: function() {
-          this.$io = io;
+          this.$io = vueSails;
         }
-      })
+      });
     }
   };
 
   if(typeof exports === 'object' && typeof module === 'object') {
-    module.exports = plugin
+    module.exports = plugin;
   } else if(typeof define === 'function' && define.amd) {
-    define(function () { return plugin })
+    define(function () { return plugin; });
   } else if (typeof window !== 'undefined') {
-    window.VueAsyncData = plugin
+    window.VueAsyncData = plugin;
   }
 })();
